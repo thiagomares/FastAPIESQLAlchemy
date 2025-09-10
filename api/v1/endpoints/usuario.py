2a4220939e1833308d578e58ec178cd669ef7428
Thiago Mares <thiagofmares@outlook.com>
2025-09-10 14:20:51 -0300

    melhorando o codigo

--- api/v1/endpoints/usuario.py
+++ api/v1/endpoints/usuario.py
@@ -1,6 +1,6 @@
 from typing import List, Optional, Any
 
-from fastapi import APIRouter, Depends, HTTPException, status, Response
+from fastapi import APIRouter, status, Depends, HTTPException, Response
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
 
@@ -9,89 +9,109 @@ from sqlalchemy.future import select
 from sqlalchemy.exc import IntegrityError
 
 from models.usuario_model import UsuarioModel
-from schemas.usuario_schema import UsuarioSchemaBase, UsuarioSchemaCreate, UsuarioSchemaUpdate
+from schemas.usuario_schema import *
 from core.deps import get_session, get_current_user
 from core.security import gerar_hash_senha
 from core.auth import autenticar, criar_token_acesso
 
+
 router = APIRouter()
 
-# Get Logado
-@router.get("/logado", response_model=UsuarioSchemaBase)
-def get_logado(usuario_logado: UsuarioSchemaBase = Depends(get_current_user)):
+
+# GET Logado
+@router.get('/logado', response_model=UsuarioSchemaBase)
+def get_logado(usuario_logado: UsuarioModel = Depends(get_current_user)):
     return usuario_logado
 
-# Sign Up
-@router.post("/signup", status_code=status.HTTP_201_CREATED, response_model=UsuarioSchemaBase)
-async def create_usuario(usuario: UsuarioSchemaCreate, session: AsyncSession = Depends(get_session)):
-    try:
-        async with session as sess:
-            usuario_db = UsuarioModel(nome=usuario.nome, email=usuario.email, senha=gerar_hash_senha(usuario.senha), ativo=usuario.ativo)
-            sess.add(usuario_db)
-            await sess.commit()
-            await sess.refresh(usuario_db)
-            return usuario_db
-    except IntegrityError:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Usuário já cadastrado")
-    except:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao inserir o usuário")
 
-    
-# Get All
-@router.get("/usuarios", response_model=List[UsuarioSchemaBase])
-async def get_usuarios(session: AsyncSession = Depends(get_session)):
-    try:
-        async with session as sess:
-            query = select(UsuarioModel)
-            result = await sess.execute(query)
-            usuarios = result.scalars().unique().all()
-            return usuarios
-    except IntegrityError:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao buscar os usuários")
-    except:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao buscar os usuários")
+# POST / Signup
+@router.post('/signup', status_code=status.HTTP_201_CREATED, response_model=UsuarioSchemaBase)
+async def post_usuario(usuario: UsuarioSchemaCreate, db: AsyncSession = Depends(get_session)):
+    novo_usuario: UsuarioModel = UsuarioModel(nome=usuario.nome,
+                                              email=usuario.email, senha=gerar_hash_senha(usuario.senha), ativo=usuario.ativo)
+    async with db as session:
+        try:
+            session.add(novo_usuario)
+            await session.commit()
 
-# Get One
-@router.get("/usuarios/{usuario_id}", response_model=UsuarioSchemaBase)
-async def get_usuario(usuario_id: int, session: AsyncSession = Depends(get_session)):
-    try:
-        async with session as sess:
-            query = select(UsuarioModel).filter(UsuarioModel.id == usuario_id)
-            result = await sess.execute(query)
-            usuario = result.scalars().unique().one_or_none()
-            if not usuario:
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado")
+            return novo_usuario
+        except IntegrityError:
+            raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                                detail='Já existe um usuário com este email cadastrado.')
+
+
+# GET Usuarios
+@router.get('/', response_model=List[UsuarioSchemaBase])
+async def get_usuarios(db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(UsuarioModel)
+        result = await session.execute(query)
+        usuarios: List[UsuarioSchemaBase] = result.scalars().unique().all()
+
+        return usuarios
+
+
+# GET Usuario
+@router.get('/{usuario_id}', response_model=UsuarioSchemaArtigos, status_code=status.HTTP_200_OK)
+async def get_usuario(usuario_id: int, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(UsuarioModel).filter(UsuarioModel.id == usuario_id)
+        result = await session.execute(query)
+        usuario: UsuarioSchemaArtigos = result.scalars().unique().one_or_none()
+
+        if usuario:
             return usuario
-    except:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao buscar o usuário")
-    
+        else:
+            raise HTTPException(detail='Usuário não encontrado.',
+                                status_code=status.HTTP_404_NOT_FOUND)
 
 
-# Update
-@router.put("/usuarios/{usuario_id}", response_model=UsuarioSchemaBase)
-async def update_usuario(usuario_id: int, usuario: UsuarioSchemaUpdate, session: AsyncSession = Depends(get_session)):
-    try:
-        async with session as sess:
-            query = select(UsuarioModel).filter(UsuarioModel.id == usuario_id)
-            result = await sess.execute(query)
-            usuario_db: UsuarioModel = result.scalars().unique().one_or_none()
-            if not usuario_db:
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado")
-            else:
-                try:
-                    usuario_db.nome = usuario.nome
-                    usuario_db.email = usuario.email
-                    usuario_db.senha = gerar_hash_senha(usuario.senha)
-                    await sess.commit()
-                    await sess.refresh(usuario_db)
-                except IntegrityError:
-                    await sess.rollback()
-                    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao atualizar o usuário")
-                finally:
-                    return usuario_db
-    except IntegrityError:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao atualizar o usuário")
-    
+# PUT Usuario
+@router.put('/{usuario_id}', response_model=UsuarioSchemaBase, status_code=status.HTTP_202_ACCEPTED)
+async def put_usuario(usuario_id: int, usuario: UsuarioSchemaUpdate, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(UsuarioModel).filter(UsuarioModel.id == usuario_id)
+        result = await session.execute(query)
+        usuario_up: UsuarioSchemaBase = result.scalars().unique().one_or_none()
+
+        if usuario_up:
+            if usuario.nome:
+                usuario_up.nome = usuario.nome
+            if usuario.sobrenome:
+                usuario_up.sobrenome = usuario.sobrenome
+            if usuario.email:
+                usuario_up.email = usuario.email
+            if usuario.eh_admin:
+                usuario_up.eh_admin = usuario.eh_admin
+            if usuario.senha:
+                usuario_up.senha = gerar_hash_senha(usuario.senha)
+
+            await session.commit()
+
+            return usuario_up
+        else:
+            raise HTTPException(detail='Usuário não encontrado.',
+                                status_code=status.HTTP_404_NOT_FOUND)
+
+
+# DELETE usuario
+@router.delete('/{usuario_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_usuario(usuario_id: int, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(UsuarioModel).filter(UsuarioModel.id == usuario_id)
+        result = await session.execute(query)
+        usuario_del: UsuarioSchemaArtigos = result.scalars().unique().one_or_none()
+
+        if usuario_del:
+            await session.delete(usuario_del)
+            await session.commit()
+
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:
+            raise HTTPException(detail='Usuário não encontrado.',
+                                status_code=status.HTTP_404_NOT_FOUND)
+
+
 # POST Login
 @router.post('/login')
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_session)):
